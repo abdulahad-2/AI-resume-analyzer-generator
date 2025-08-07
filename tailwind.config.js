d815460c29279676640670778cc00691cc5deee5
Abdul Ahad <ahadking6152@gmail.com>
2025-08-07 09:22:48 -0700

    edited tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,46 +6,46 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      white: "#ffffff",
-      black: "#000000",
-      slate: {
-        100: "#f1f5f9",
-        200: "#e2e8f0",
-        300: "#cbd5e1",
-        600: "#475569",
-        700: "#334155",
-        800: "#1e293b",
-      },
-      blue: {
-        600: "#2563eb",
-        700: "#1d4ed8",
-        900: "#1e3a8a",
-      },
-      purple: {
-        500: "#a21caf",
-        600: "#86198f",
-        700: "#701a75",
-        800: "#581c87",
-        900: "#3b0764",
-      },
-      gray: {
-        50: "#f9fafb",
-        100: "#f3f4f6",
-        200: "#e5e7eb",
-        300: "#d1d5db",
-        400: "#9ca3af",
-        500: "#6b7280",
-        600: "#4b5563",
-        700: "#374151",
-        800: "#1f2937",
-        900: "#111827",
-      },
-      // Add other colors as needed
-    },
     extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        black: "#000000",
+        slate: {
+          100: "#f1f5f9",
+          200: "#e2e8f0",
+          300: "#cbd5e1",
+          600: "#475569",
+          700: "#334155",
+          800: "#1e293b",
+        },
+        blue: {
+          600: "#2563eb",
+          700: "#1d4ed8",
+          900: "#1e3a8a",
+        },
+        purple: {
+          500: "#a21caf",
+          600: "#86198f",
+          700: "#701a75",
+          800: "#581c87",
+          900: "#3b0764",
+        },
+        gray: {
+          50: "#f9fafb",
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          400: "#9ca3af",
+          500: "#6b7280",
+          600: "#4b5563",
+          700: "#374151",
+          800: "#1f2937",
+          900: "#111827",
+        },
+        // Add other colors as needed
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
